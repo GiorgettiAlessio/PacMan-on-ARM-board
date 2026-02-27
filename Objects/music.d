e2ea16c8b07e0e194fc17186aa629278567f1637
GiorgettiAlessio <64132389+GiorgettiAlessio@users.noreply.github.com>
2026-02-27 16:09:53 +0100

    Add files via upload

---
+++ Objects/music.d
@@ -0,0 +1,5 @@
+./objects/music.o: Source\music\music.c Source\music\music.h \
+  C:\Users\aless\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h \
+  Source\CMSIS_core\core_cm3.h \
+  C:\Users\aless\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h \
+  Source\timer\timer.h
---
+++ Objects/sample.d
@@ -0,0 +1,6 @@
+./objects/sample.o: Source\sample.c \
+  C:\Users\aless\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h \
+  Source\CMSIS_core\core_cm3.h \
+  C:\Users\aless\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h \
+  Source\GLCD\GLCD.h Source\TouchPanel\TouchPanel.h Source\timer\timer.h \
+  Source\joystick\joystick.h Source\RIT\RIT.h
